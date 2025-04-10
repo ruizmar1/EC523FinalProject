507d3d06432816687484f09750185981a4ba753f
ruizmar1 <MarissaRuiz2003@gmail.com>
2025-04-10 13:55:44 -0400

    Update marissa_try.py

--- homework/marissa_try.py
+++ homework/marissa_try.py
@@ -2,11 +2,12 @@ from kart_env import SuperTuxKartEnv
 
 env = SuperTuxKartEnv(track='lighthouse')
 obs, _ = env.reset()
-
+t = 0
 done = False
 while not done:
     action = env.action_space.sample()  # Replace with your controller or RL agent
     obs, reward, terminated, truncated, _ = env.step(action)
     done = terminated or truncated
+    t += 1
     env.render()
-print("done!")
+print("Finished at t=", t)
