8b190bc6975963f4538d0a222f3d6c6909be6e15
ruizmar1 <MarissaRuiz2003@gmail.com>
2025-04-10 22:17:24 -0400

    Update controller.py

--- homework/controller.py
+++ homework/controller.py
@@ -1,24 +1,84 @@
 import pystk
 
-
-def control(aim_point, current_vel, steer_gain=6, skid_thresh=0.2, target_vel=25):
+def control(aim_point, current_vel, steer_gain=6, skid_thresh=0.2, target_vel=80):
     import numpy as np
     #this seems to initialize an object
     action = pystk.Action()
+
+    # Trying to find the radian angle given by the aim point and scale that to the scale [-1, 1]
+    if aim_point[0]<0:
+        if aim_point[1]>0:
+            action.drift = True
+            action.steer = -0.9
+        else:
+            if aim_point[0]<-0.5:
+                if aim_point[1]>-0.5:
+                    action.drift = True
+            else:
+                action.drift = False
+            
+            if aim_point[0]>-0.03:
+                action.steer = 0
+                target_vel = 80
+            elif aim_point[0]>-0.2:
+                action.steer = -0.8
+                target_vel = 80
+            else:
+                action.steer = -0.9
+                target_vel = 30
+    else:
+        if aim_point[1]>0:
+            action.drift = True
+            action.steer = 0.9
+        else:
+            if aim_point[0]>0.5:
+                if aim_point[1]>-0.5:
+                    action.drift = True
+            else:
+                action.drift = False
+            
+            if aim_point[0]<0.03:
+                action.steer = 0
+                target_vel = 80
+            elif aim_point[0]<0.2:
+                action.steer = 0.8
+                target_vel = 80
+            else:
+                action.steer = 0.9
+                target_vel = 30
+
+    # set acceleration and break to reach target velocity
+    if current_vel < target_vel:
+        action.brake = False
+        if (current_vel/target_vel) < 0.5:
+            if abs(aim_point[0])<0.1:
+                action.nitro = False
+        else:
+            action.nitro = False
+        action.acceleration = 1 - current_vel/target_vel
+    elif current_vel > target_vel:
+        action.brake= True
+
+    return action
+
+# def control(aim_point, current_vel, steer_gain=6, skid_thresh=0.2, target_vel=25):
+#     import numpy as np
+#     #this seems to initialize an object
+#     action = pystk.Action()
 
    
  
     
 
-    #compute acceleration
-    action.acceleration = 0.1
+#     #compute acceleration
+#     action.acceleration = 0.1
     
 
         
 
     
 
-    return action
+#     return action
 
     
 
